6d58324a7bd4500899eb6e1b7dc2fc652b56dd03
Raviteja444 <pagidojuraviteja2@gmail.com>
2020-01-01 06:46:49 +0530

    pushing to remote

---
+++ EmailNotificationService/Controllers/ValuesController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+using MyCompany.Messaging;
+
+namespace EmailNotificationService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromQuery] string value)
+        {
+            string rabbitMqAddress = "rabbitmq://localhost:5672";
+            string rabbitMqQueue = "mycompany.domains.queues";
+            Uri rabbitMqRootUri = new Uri(rabbitMqAddress);
+
+            IBusControl rabbitBusControl = Bus.Factory.CreateUsingRabbitMq(rabbit =>
+            {
+                rabbit.Host(rabbitMqRootUri, settings =>
+                {
+                    settings.Password("guest");
+                    settings.Username("guest");
+                });
+            });
+
+            Task<ISendEndpoint> sendEndpointTask = rabbitBusControl.GetSendEndpoint(new Uri(string.Concat(rabbitMqAddress, "/", rabbitMqQueue)));
+            ISendEndpoint sendEndpoint = sendEndpointTask.Result;
+
+            Task sendTask = sendEndpoint.Send<IRegisterCustomer>(new
+            {
+                
+                Name = value
+              
+            });
+            Console.ReadKey();
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ EmailNotificationService/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using MassTransit;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace EmailNotificationService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+
+        private static void RunMassTransitPublisherWithRabbit()
+        {
+            
+        }
+    }
+}
